54bb34c97c471fb836e5cae58a5a76c3ba8b928b
Abdul Rehman <ar03353505910@gmail.com>
2025-03-06 09:43:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def check_breach(password):
 st.title("🔒 Password Manager")
 
 # Display Current Time and Temperature
-st.header("📅 Current Time & Temperature - Karachi")
+st.header("Karachi")
 karachi_tz = pytz.timezone("Asia/Karachi")
 current_time = datetime.now(karachi_tz).strftime("%I:%M %p, %d %B %Y")
 st.markdown(f"<div style='text-align: center; font-size: 24px;'><b>🕒 {current_time}</b></div>", unsafe_allow_html=True)
